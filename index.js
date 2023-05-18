1ac02881410c43ac19debfdaea6e9e5b7d0d46a4
aagocloud <114477644+aagocloud@users.noreply.github.com>
2023-05-18 10:29:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+async function run() {
+  try {
+    const placeholders = process.env.INPUT_PLACEHOLDERS;
+    const files = process.env.INPUT_FILES;
+
+    const placeholderPairs = placeholders.split('\n');
+    const filePaths = files.split('\n');
+
+    for (const filePath of filePaths) {
+      console.log(`Replacing placeholders in ${filePath}`);
+
+      let fileContent = fs.readFileSync(filePath, 'utf8');
+      
+      for (const placeholderPair of placeholderPairs) {
+        const [key, value] = placeholderPair.split('=');
+        const regex = new RegExp(`\\\$${key}`, 'g');
+        fileContent = fileContent.replace(regex, value);
+      }
+
+      fs.writeFileSync(filePath, fileContent);
+    }
+
+    console.log('Placeholders replaced successfully');
+  } catch (error) {
+    console.error('An error occurred:', error);
+    process.exit(1);
+  }
+}
+
+run();
